7841e4c0cbf957501e2815175c0f7f263da4e8fa
rizpedia <achmadrizkiramadhan0101@gmail.com>
2024-05-08 16:42:33 +0700

    Update UI components in verify-otp page

--- src/app/auth/verify-otp/page.js
+++ src/app/auth/verify-otp/page.js
@@ -20,8 +20,8 @@ function page() {
   return (
     <>
       <Navbar />
-      <div className="flex min-h-screen w-full items-center justify-center bg-background py-12 px-4 sm:px-6 lg:px-8 ">
-        <Card className="w-full max-w-md space-y-8 rounded-lg bg-background p-8 shadow-lg ">
+      <div className="flex min-h-screen w-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+        <Card className="w-full max-w-md space-y-8 rounded-lg bg-secondary p-8 shadow-lg ">
           <div>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-card-foreground dark:text-card-foreground">
               Masukan Kode Verifikasi
