d2462e87dbf72c2f5201e166c7742fe7898b9ae4
rizpedia <achmadrizkiramadhan0101@gmail.com>
2024-05-08 16:39:27 +0700

    Update UI components in login page

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,18 +1,16 @@
-import React from "react";
-import Link from "next/link";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Image from "next/image";
-import { Card } from "@/components/ui/card";
 import Navbar from "@/components/group/navbar";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 function page() {
   return (
     <>
       <Navbar />
-      <div className="flex min-h-screen w-full items-center justify-center bg-background py-12 px-4 sm:px-6 lg:px-8 ">
-        <Card className="w-full max-w-md space-y-8 rounded-lg bg-background p-8 shadow-lg ">
+      <div className="flex min-h-screen w-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+        <Card className="w-full max-w-md space-y-8 rounded-lg bg-secondary p-8 shadow-lg ">
           <div>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-card-foreground dark:text-card-foreground">
               Sign In Account
