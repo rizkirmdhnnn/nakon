be02dc102d4d58467647f2ba910b9e381ff16660
aaneverything <aanvinanta5@gmail.com>
2024-05-09 12:04:37 +0700

    membuat popup sembere selengkapnya dan lain lain

--- src/components/group/msgcard.jsx
+++ src/components/group/msgcard.jsx
@@ -1,15 +1,17 @@
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Calendar } from "lucide-react";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Calendar, X } from "lucide-react";
 import Link from "next/link";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import React from "react";
 
 export default function MsgCard({ question, date, message, id }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleButtonClick = () => {
+    setOpen(!open);
+    console.log(open);
+  };
   return (
     <Card x-chunk="dashboard-01-chunk-1" className="bg-secondary border-none">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -25,10 +27,34 @@ export default function MsgCard({ question, date, message, id }) {
         <p className="text-1xl text-muted-foreground">{message}</p>
       </CardContent>
       <CardFooter className="float-end">
-        <Button>
-          <Link href={`#${id}`}>Selengkapnya</Link>
+        <Button onClick={handleButtonClick}>
+          Selengkapnya
+          {/* <Link href={`#${id}`}>Selengkapnya</Link> */}
         </Button>
       </CardFooter>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <div className="flex justify-between items-center">
+              <div>
+                <AlertDialogTitle>{question}</AlertDialogTitle>
+              </div>
+              <X className="w-4 cursor-pointer" onClick={handleButtonClick} />
+            </div>
+            <AlertDialogDescription>{message}</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            {/* <AlertDialogCancel>Tandai Sudah Dibaca</AlertDialogCancel>
+            <AlertDialogAction>Hapus</AlertDialogAction> */}
+            <div className="flex justify-between w-full gap-3">
+              <Button className="w-1/2">Tandai Sudah Dibaca</Button>
+              <Button variant="outlineDestructive" className="w-1/2">
+                Hapus
+              </Button>
+            </div>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </Card>
   );
 }
