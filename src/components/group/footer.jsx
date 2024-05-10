af42be25b3c14a33e6d21d842fc35fcf07d2e397
aaneverything <aanvinanta5@gmail.com>
2024-05-10 18:38:48 +0700

    update warna light mode

--- src/components/group/footer.jsx
+++ src/components/group/footer.jsx
@@ -14,7 +14,7 @@ function CustomFooter() {
             </div>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
               <li>
-                <a href="#" className="hover:underline me-4 md:me-6">
+                <a href="#" className=" hover:underline me-4 md:me-6">
                   Profile
                 </a>
               </li>
