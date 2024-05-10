10b2c05e4c3fa9bacdc9e905a6c11011871f38a1
aaneverything <aanvinanta5@gmail.com>
2024-05-10 16:44:49 +0700

    fix button

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -11,9 +11,8 @@ export default function NotFound() {
           <div class="flex flex-col items-center max-w-sm mt-10 mx-auto text-center">
             <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Halaman Tidak Ditemukan</h1>
             <p class="mt-4 text-gray-500 dark:text-gray-400">Duh, Halaman Yang Kamu Cari Tidak Ditemukan</p>
-            <div class="flex items-center w-full mt-6 gap-x-3 shrink-0 ">
-              <Button variant="" className="bg-muted text-white hover:bg-slate-900 flex items-center justify-center w-1/2 px-5 py-2 text-sm rounded-lg gap-x-2 ">Kembali</Button>
-              <Button className="w-1/2 px-5 py-2 text-sm  bg-primary rounded-lg shrink-0 ">Ke Home</Button>
+            <div class="flex items-center mt-6 ">
+              <Button className=" w-auto px-5 py-2 text-sm  bg-primary rounded-lg shrink-0 ">Ke Home</Button>
             </div>
           </div>
         </div>
