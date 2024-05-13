6dbba3a310a6237230ae6a040c2a694c32f90f92
rizpedia <achmadrizkiramadhan0101@gmail.com>
2024-05-13 20:17:51 +0700

    fix module not found

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,19 @@
-import CardFeature from "@/components/group/CardFeature";
-import Navbar from "@/components/group/navbar";
-import Image from "next/image";
-import Hero from "@/components/group/hero";
-import Stats from "@/components/group/stats";
-import CustomFooter from "@/components/group/footer";
 import Feature from "@/components/group/Feature";
+import CustomFooter from "@/components/group/footer";
+import Hero from "@/components/group/hero";
+import Navbar from "@/components/group/navbar";
 import ShareCode from "@/components/group/shareCode";
+import Stats from "@/components/group/stats";
 
 export default function Home() {
   return (
     <>
-     <Navbar/>
-     <Hero/>
-     <Feature/>
-     <Stats/>
-     <ShareCode/>
-     <CustomFooter/>
+      <Navbar />
+      <Hero />
+      <Feature />
+      <Stats />
+      <ShareCode />
+      <CustomFooter />
     </>
   );
 }
