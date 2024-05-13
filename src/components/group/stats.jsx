7984d29744d9b05c076c68ac59749f0ce24f1576
aaneverything <aanvinanta5@gmail.com>
2024-05-13 19:58:08 +0700

    update

--- src/components/group/stats.jsx
+++ src/components/group/stats.jsx
@@ -9,7 +9,7 @@ function Stats() {
       <div className="mx-24 mb-60 border border-gray-700 dark:border-gray-600x rounded-lg md:p-[100px] " id="stats" role="tabpanel" aria-labelledby="stats-tab">
         <dl className="grid md:grid-cols-3 lg:grid-cols-3 gap-10 max-w-screen-xl grid-cols-1 p-4 mx-auto text-gray-900  dark:text-white">
           <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400 ">73M+</dt>
+            <dt className="mb-2 text-3xl font-extrabold  text-gray-500 dark:text-gray-400 ">73M+</dt>
             <dd className="text-gray-500 dark:text-gray-400">Pengunjung</dd>
           </div>
           <div className="flex flex-col items-center justify-center">
