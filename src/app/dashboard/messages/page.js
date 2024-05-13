5c4a9ae67294cb4ee4f8e578882cfd47f88be945
aaneverything <aanvinanta5@gmail.com>
2024-05-13 15:12:41 +0700

    fix: min-screen for md

--- src/app/dashboard/messages/page.js
+++ src/app/dashboard/messages/page.js
@@ -3,22 +3,16 @@
 import CustomFooter from "@/components/group/footer";
 import MsgCard from "@/components/group/msgcard";
 import Navbar from "@/components/group/navbar";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 function page() {
   return (
     <>
       <Navbar />
-      <main className="p-5 md:px-[100px] xl:px-[250px]  pt-[100px] h-screen">
+      <main className="p-5 md:px-[100px] xl:px-[250px]  pt-[100px] min-h-screen">
         <div className="flex justify-between items-center">
           <div>
             <h1 className="text-2xl font-bold">Semua Pesan</h1>
-            <p className="text-base leading-10">Lihat semua pesan masuk</p>
+            <p className="text-base md:leading-10 leading-[20px]">Lihat semua pesan masuk</p>
           </div>
           <Select>
             <SelectTrigger className="w-[180px]">
@@ -31,59 +25,17 @@ function page() {
           </Select>
         </div>
         <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 mt-4">
-          <MsgCard
-            question={"Pertanyaan #1"}
-            date={"12 oktober 2024"}
-            message={
-              "hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"
-            }
-            id={"1"}
-          />
+          <MsgCard question={"Pertanyaan #1"} date={"12 oktober 2024"} message={"hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"} id={"1"} />
 
-          <MsgCard
-            question={"Pertanyaan #2"}
-            date={"12 oktober 2024"}
-            message={
-              "hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"
-            }
-            id={"2"}
-          />
+          <MsgCard question={"Pertanyaan #2"} date={"12 oktober 2024"} message={"hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"} id={"2"} />
 
-          <MsgCard
-            question={"Pertanyaan #3"}
-            date={"12 oktober 2024"}
-            message={
-              "hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"
-            }
-            id={"3"}
-          />
+          <MsgCard question={"Pertanyaan #3"} date={"12 oktober 2024"} message={"hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"} id={"3"} />
 
-          <MsgCard
-            question={"Pertanyaan #4"}
-            date={"12 oktober 2024"}
-            message={
-              "hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"
-            }
-            id={"4"}
-          />
+          <MsgCard question={"Pertanyaan #4"} date={"12 oktober 2024"} message={"hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"} id={"4"} />
 
-          <MsgCard
-            question={"Pertanyaan #5"}
-            date={"12 oktober 2024"}
-            message={
-              "hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"
-            }
-            id={"5"}
-          />
+          <MsgCard question={"Pertanyaan #5"} date={"12 oktober 2024"} message={"hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"} id={"5"} />
 
-          <MsgCard
-            question={"Pertanyaan #6"}
-            date={"12 oktober 2024"}
-            message={
-              "hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"
-            }
-            id={"6"}
-          />
+          <MsgCard question={"Pertanyaan #6"} date={"12 oktober 2024"} message={"hallo ini merupakan contoh pesan yang nantinya akan dikirim. ini  whanya pesan dummy saja ya"} id={"6"} />
         </div>
       </main>
       <CustomFooter />
