a8dfc195d71d562136e84edd2cbb355c8f13ca6e
rizpedia <achmadrizkiramadhan0101@gmail.com>
2024-05-15 20:46:27 +0700

    chore: Update styling for group stats component

--- src/components/group/stats.jsx
+++ src/components/group/stats.jsx
@@ -1,38 +1,59 @@
-import React from "react";
+import { Card } from "@/components/ui/card";
 
 function Stats() {
   return (
     <>
-      <div className="pt-[10px]">
-        <h3 className="my-20 text-4xl pt-28 font-bold font-poppins text-center ">Statistik</h3>
-      </div>
-      <div className="mx-24 mb-60 border border-gray-700 dark:border-gray-600x rounded-lg md:p-[100px] " id="stats" role="tabpanel" aria-labelledby="stats-tab">
-        <dl className="grid md:grid-cols-3 lg:grid-cols-3 gap-10 max-w-screen-xl grid-cols-1 p-4 mx-auto text-gray-900  dark:text-white">
-          <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl font-extrabold  text-gray-500 dark:text-gray-400 ">73M+</dt>
-            <dd className="text-gray-500 dark:text-gray-400">Pengunjung</dd>
+      <div className="px-6 py-3  md:px-[100px]  xl:px-[250px] mt-[200px]">
+        <div className="flex flex-col justify-center text-center items-center">
+          <div className="my-10">
+            <h1 className="font-bold text-4xl mb-5 text-primary">Statistik</h1>
+            <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400 items">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sit
+              voluptatem laudantium, quia sunt iure illo possimus odit et omnis
+              id saepe quae a, magnam iste nam doloremque nihil optio?
+            </p>
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">10M+</dt>
-            <dd className="text-gray-500 dark:text-gray-400">Pertanyaan</dd>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">10M+</dt>
-            <dd className="text-gray-500 dark:text-gray-400">Akun</dd>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">1B+</dt>
-            <dd className="text-gray-500 dark:text-gray-400">Kontributor</dd>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">90+</dt>
-            <dd className="text-gray-500 dark:text-gray-400">Sponsor</dd>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">4M+</dt>
-            <dd className="text-gray-500 dark:text-gray-400">Organisasi</dd>
-          </div>
-        </dl>
+        </div>
+        <Card className="px-6 py-3 md:px-[100px] xl:px-[250px]">
+          <dl className="grid md:grid-cols-3 lg:grid-cols-3 gap-10 grid-cols-1 p-4 mx-auto ">
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl font-extrabold  text-gray-500 dark:text-gray-400 ">
+                73M+
+              </dt>
+              <dd className="text-gray-500 dark:text-gray-400">Pengunjung</dd>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">
+                10M+
+              </dt>
+              <dd className="text-gray-500 dark:text-gray-400">Pertanyaan</dd>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">
+                10M+
+              </dt>
+              <dd className="text-gray-500 dark:text-gray-400">Akun</dd>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">
+                1B+
+              </dt>
+              <dd className="text-gray-500 dark:text-gray-400">Kontributor</dd>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">
+                90+
+              </dt>
+              <dd className="text-gray-500 dark:text-gray-400">Sponsor</dd>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl font-extrabold text-gray-500 dark:text-gray-400">
+                4M+
+              </dt>
+              <dd className="text-gray-500 dark:text-gray-400">Organisasi</dd>
+            </div>
+          </dl>
+        </Card>
       </div>
     </>
   );
