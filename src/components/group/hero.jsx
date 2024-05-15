f44c98b32230c7bb5c8a39d4d5fdc5cd286dfcc6
rizpedia <achmadrizkiramadhan0101@gmail.com>
2024-05-15 20:46:38 +0700

    chore: Update hero component styling and add button with link to login page

--- src/components/group/hero.jsx
+++ src/components/group/hero.jsx
@@ -1,28 +1,30 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowRightCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
 import Navbar from "./navbar";
-import { Button } from "@/components/ui/button";
 
 function hero() {
   return (
     <>
       <Navbar />
-      <div className="flex items-center flex-col justify-center pt-[200px]">
+      <div className="flex items-center flex-col justify-center pt-[200px] px-6 py-3 md:px-[100px] xl:px-[250px] ">
         <Image src={"/sembereeee.png"} width={600} height={200} />
-        <div className="justify-center space-y-4">
-          <div className="">
-            <h1 className="my-20 text-4xl pt-5 text-primary font-bold font-poppins text-center">Takonwae - Tempat bertanya tanpa takut ketahuan siapa kamu</h1>
-            <p className="text-base sm:text-xl  font-poppins text-center">Kalo orang cupu make platform ini biasanya buat ngungkapin perasaan si, semoga kalian tidak</p>
-          </div>
+        <div>
+          <h1 className="mt-20 mb-5 text-4xl pt-5 text-primary font-bold font-poppins text-center">
+            Ini jargonnya nanti
+          </h1>
+          <p className="text-base sm:text-xl text-center">
+            Kalo orang cupu make platform ini biasanya buat ngungkapin perasaan
+            si, semoga kalian tidak
+          </p>
         </div>
-      </div>
-      <div class="h-[50px]"></div>
-      <div class="flex justify-center px-[50px] pt-[50px]">
-        <div className="flex justify-start py-4">
-          <Button>Get Started</Button>
-        </div>
+        <Button asChild size="sm" className="mx-auto gap-1 mt-8">
+          <Link href="/auth/login">
+            View All
+            <ArrowRightCircle className="h-4 w-4" />
+          </Link>
+        </Button>
       </div>
     </>
   );
