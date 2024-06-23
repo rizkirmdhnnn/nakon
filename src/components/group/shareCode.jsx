b9c26a37afc3d3dc8d6a465f1ba92da6996235e8
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-06-23 18:33:59 +0700

    fix: missing route
    kocak malah ngarah ke login

--- src/components/group/shareCode.jsx
+++ src/components/group/shareCode.jsx
@@ -8,15 +8,15 @@ function ShareCode() {
       <div className="flex items-center flex-col justify-center pt-[200px] px-6 py-3 md:px-[100px] xl:px-[250px] ">
         <div>
           <h1 className="mb-5 text-4xl pt-5 text-primary font-bold font-poppins text-center">
-           Open Source Code 
+            Open Source Code
           </h1>
           <p className="text-base sm:text-xl text-center">
-           Open source code, please use this code for good purposes
+            Open source code, please use this code for good purposes
           </p>
         </div>
         <Button asChild size="sm" className="mx-auto gap-2 mt-8">
-          <Link href="/auth/login">
-            go to code
+          <Link href="https://github.com/rizkirmdhnnn/nakon">
+            Go to code
             <ArrowRightCircle className="h-4 w-4" />
           </Link>
         </Button>
