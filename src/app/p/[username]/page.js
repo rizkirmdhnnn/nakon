6b207dd907f1d9d78d2e72bf351633d45b1f5ed4
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-06-24 11:03:22 +0700

    feat: send question now is working
    masih make alert, next lah dibenerin

--- src/app/p/[username]/page.js
+++ src/app/p/[username]/page.js
@@ -1,15 +1,87 @@
+"use client";
 import CustomFooter from "@/components/group/footer";
 import Navbar from "@/components/group/navbar";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState, useRef } from "react";
+import NotFound from "@/app/not-found";
 
-export default function Home({ params }) {
+export default function Question({ params }) {
+  const [user, setUser] = useState({});
+  const dataFetchedRef = useRef(false);
+  const [errorFetch, setErrorFetch] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getData = async () => {
+      if (dataFetchedRef.current) return;
+      dataFetchedRef.current = true;
+
+      try {
+        const res = await fetch(
+          `https://nakonapi.rizpedia.com/api/v1/user/?username=${params.username}`,
+        );
+        const data = await res.json();
+
+        if (res.ok) {
+          setUser(data.data);
+          console.log("Fetch response:", data);
+        } else {
+          setErrorFetch(true);
+        }
+      } catch (error) {
+        setErrorFetch(true);
+      }
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary" />
+      </div>
+    );
+  }
+
+  if (errorFetch) {
+    return <NotFound />;
+  }
+
+  const sendQuestion = async (e) => {
+    e.preventDefault();
+    const chat = document.getElementById("chat").value;
+    if (!chat) {
+      alert("Pertanyaan tidak boleh kosong");
+      return;
+    }
+    try {
+      const res = await fetch("https://nakonapi.rizpedia.com/api/v1/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: chat,
+          user_id: user.id,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        alert("Pertanyaan berhasil dikirim");
+      }
+    } catch (error) {}
+  };
+
+  console.log(user);
   return (
     <>
       <Navbar />
       <div className="flex flex-row justify-between px-[50px] pb-[50px] md:px-[170px] md:pb-[127px] py-60">
         <div className="bg-transparent w-full rounded-lg md:rounded-l-lg  md:px-[97px] md:pb-[73px] flex flex-col justify-between">
-          <div class="flex justify-center">
+          <div className="flex justify-center">
             <Avatar className="w-[100px] h-[100px] flex">
               <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
               <AvatarFallback>CN</AvatarFallback>
@@ -20,7 +92,7 @@ export default function Home({ params }) {
             Tanyakan kepada {params.username}
           </h3>
           <p className="font-bold mb-2">Pertanyaan</p>
-          <form>
+          <form onSubmit={sendQuestion}>
             <textarea
               id="chat"
               rows="10"
@@ -35,7 +107,6 @@ export default function Home({ params }) {
           </form>
         </div>
       </div>
-
       <CustomFooter />
     </>
   );
