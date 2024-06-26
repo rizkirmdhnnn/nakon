05c07eaf64f0934f65238065de2a7651b02097aa
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-06-26 19:33:57 +0700

    refactor: missing image metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,33 +7,32 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Nakoni opo wae | Nakon",
-  description: "Share your link and give it to people",
+  description: "Share your link and give it to people to get surprised",
   metadataBase: "nakon.vercel.app",
   robots: {
     index: true,
     follow: true,
   },
   openGraph: {
-    description: "Share your link and give it to people",
+    description: "Share your link and give it to people to get surprised",
     siteName: "nakon.vercel.app",
     title: "Nakoni opo wae | Nakon",
     url: "https://nakon.vercel.app",
     images: [
       {
-        url: "http://localhost:3000/_next/image?url=%2Fsembereeee.png&w=640&q=75",
+        url: "http://localhost:3000/sembereeee.png",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Nakoni opo wae | Nakon",
-    description:
-      "Kumpulkan berbagai pertanyaan dari siapa saja secara anonim melaui aplikasi TanyaAja. Mudah, gratis dan terjamin rahasia.",
+    description: "Share your link and give it to people to get surprised",
     creator: "@rizkirmdhnn",
     site: "nako.vercel.app",
     images: [
       {
-        url: "http://localhost:3000/_next/image?url=%2Fsembereeee.png&w=640&q=75",
+        url: "http://localhost:3000/sembereeee.png",
       },
     ],
   },
