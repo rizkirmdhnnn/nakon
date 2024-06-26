a8e54c9bf9952efa27570dd63be872d218da3077
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-06-26 19:07:24 +0700

    feat: forgot password now is working
    belum dites lebih lanjut

--- src/app/auth/reset-password/change/page.js
+++ src/app/auth/reset-password/change/page.js
@@ -1,75 +1,86 @@
-
 "use client";
 
 import Navbar from "@/components/group/navbar";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2 } from "lucide-react";
-import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
 
 function RegisterPage() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    username: "",
-    email: "",
-    password: "",
-    name: "balskdfj",
-    password_confirmation: "rizkirmdhn",
-  });
-  const [error, setError] = useState(null);
-  const [open, setOpen] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+  const [token, setToken] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setFormData((prevData) => ({
-  //     ...prevData,
-  //     [name]: value,
-  //   }));
-  // };
+  useEffect(() => {
+    const storedTokenReset = localStorage.getItem("token-reset");
+    if (storedTokenReset) {
+      setToken(storedTokenReset);
+    } else {
+      router.push("/");
+    }
+  }, []);
 
-  // const handleSubmit = async (e) => {
-  //   setIsSubmitting(true);
-  //   e.preventDefault();
-  //   try {
-  //     const response = await fetch(
-  //       "https://takonwae-api.rizpedia.com/api/register",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(formData),
-  //       }
-  //     );
-  //     if (response.ok) {
-  //       setError(null);
-  //     } else {
-  //       const data = await response.json();
-  //       setError(data.message);
-  //     }
-  //   } catch (error) {
-  //     setError("Terjadi kesalahan. Silakan coba lagi.");
-  //   }
-  //   setOpen(true);
-  //   setIsSubmitting(false);
-  // };
+  const [formData, setFormData] = useState({
+    password: "",
+    password_confirm: "",
+  });
 
-  const handleModal = () => {
-    setOpen(!open);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    setIsSubmitting(true);
+    e.preventDefault();
+
+    if (formData.password !== formData.password_confirm) {
+      showToast("Error", "Passwords do not match");
+      setIsSubmitting(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://nakonapi.rizpedia.com/api/v1/auth/reset-password",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            password: formData.password,
+            token: token,
+          }),
+        },
+      );
+      const data = await response.json();
+      if (response.ok) {
+        showToast("Success", "Password has been reset successfully");
+        localStorage.removeItem("token-reset");
+        router.push("/auth/login");
+      } else {
+        showToast("Error", data.message);
+      }
+    } catch (error) {
+      showToast("Error", error.message);
+    }
+    setIsSubmitting(false);
+  };
+
+  const showToast = (title, desc) => {
+    toast({
+      title: title,
+      description: desc,
+    });
   };
 
   return (
@@ -81,75 +92,46 @@ function RegisterPage() {
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-card-foreground dark:text-card-foreground">
               Reset Password
             </h2>
-
           </div>
-          {/* <form onSubmit={handleSubmit}> */}
-          <div>
+          <form onSubmit={handleSubmit}>
             <div className="space-y-6">
               <div className="space-y-2">
                 <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
                   name="password"
-                  // value={formData.username}
-                  // onChange={handleChange}
+                  value={formData.password}
+                  onChange={handleChange}
                   required
-                  type="text"
+                  type="password"
                 />
               </div>
               <div className="space-y-2">
                 <Label htmlFor="password-confirm">Confirm Password</Label>
                 <Input
                   id="password-confirm"
-                  name="password-confirm"
-                  // value={formData.email}
-                  // onChange={handleChange}
+                  name="password_confirm"
+                  value={formData.password_confirm}
+                  onChange={handleChange}
                   required
-                  type="text"
+                  type="password"
                 />
               </div>
               <div>
                 <Button
                   className="w-full"
-                  // type="submit"
-                  // disabled={isSubmitting}
-                  onClick={handleModal}
+                  type="submit"
+                  disabled={isSubmitting}
                 >
                   {isSubmitting ? (
                     <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                   ) : (
-                    "Konfirmasi"
+                    "Change Password"
                   )}
                 </Button>
               </div>
             </div>
-          </div>
-          <AlertDialog open={open} onOpenChange={handleModal}>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                {error ? (
-                  <AlertDialogTitle>Error</AlertDialogTitle>
-                ) : (
-                  <AlertDialogTitle>Succes reset password</AlertDialogTitle>
-                )}
-                <AlertDialogDescription>
-                  {error || "Jangan lupa terus ya sembere"}
-                </AlertDialogDescription>
-                <AlertDialogFooter>
-                  {error ? (
-                    <Button variant="outline" onClick={handleModal}>
-                      Close
-                    </Button>
-                  ) : (
-                    <Link href={"/dashboard"}>
-                      {" "}
-                      <AlertDialogAction>Continue</AlertDialogAction>
-                    </Link>
-                  )}
-                </AlertDialogFooter>
-              </AlertDialogHeader>
-            </AlertDialogContent>
-          </AlertDialog>
+          </form>
         </Card>
       </div>
     </>
